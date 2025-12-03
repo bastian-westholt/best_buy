97fead17a64ef27b14f0aff591bc5d1b22fed6e1
bastian-westholt <mail@bastianwestholt.com>
2025-12-03 15:15:55 +0100

    Created interface in main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from store import Store
+import store
 import products
 
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
+# setup initial stock of inventory
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250)
                ]
+best_buy = store.Store(product_list)
 
-best_buy = Store(product_list)
-products = best_buy.get_all_products()
-print(best_buy.get_total_quantity())
-print(best_buy.order([(products[0], 1), (products[1], 2)]))
+def start(store):
+    print(f'-------# WELCOME TO BEST BUY #--------')
+    print(f'------# your electronics-store #------', end='\n\n')
+    print(f'1. List all products in store')
+    print(f'2. Show total amount in store')
+    print(f'3. Make an order')
+    print(f'4. Quit', end='\n\n')
+    user_menu_selection = input('Please choose a number: ')
+
+def main():
+    start(best_buy)
+
+if __name__ == '__main__':
+    main()
